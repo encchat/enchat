012547b9314a9f5104fc8dc2ab793c73b82aa37e
kawuka <kawuka@kawuka.xyz>
2023-01-31 21:12:21 +0100

    working flake

--- flake.nix
+++ flake.nix
@@ -1,40 +1,48 @@
 {
   description = "E2E encrypted chat";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "nixpkgs";
     flake-utils.url = github:numtide/flake-utils;
   };
   outputs = {self, nixpkgs, flake-utils, ...}@inputs:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+      libraries = with pkgs; [
+        webkitgtk
+        gtk3
+        cairo
+        gdk-pixbuf
+        glib
+        dbus
+        openssl_3
+        librsvg
+      ];
+      packages = with pkgs; [
+        curl
+        wget
+        pkg-config
+        dbus
+        openssl_3
+        glib
+        gtk3
+        libsoup
+        webkitgtk
+        nodejs-19_x
+        rust-analyzer
+        rustfmt
+        rustc
+        gcc
+        sass
+        nodePackages.pnpm
+        cargo
+      ];
     in {
       devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          nodejs-19_x
-          cargo
-          rustc
-          gcc
-          nodePackages.pnpm
-        ];
-        buildInputs = with pkgs; [
-          rustfmt
-          rust-analyzer
-          dbus
-          pkgconfig
-          openssl
-          sass
-          glib
-          cairo
-          pango
-          atk
-          gdk-pixbuf
-          libsoup
-          gtk3
-          webkitgtk
-          librsvg
-          patchelf
-        ];
+        buildInputs = packages;
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+        '';
       };
     });
 }
