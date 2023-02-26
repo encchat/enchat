88f55f18a375b4361c6f2a7462a027fdc7c8e70c
kawuka <kawuka@kawuka.xyz>
2023-02-26 14:06:04 +0100

    expose function

--- src-tauri/src/user_setup.rs
+++ src-tauri/src/user_setup.rs
@@ -10,7 +10,7 @@ use ed25519_dalek::{Keypair, Signature, PublicKey};
 use crate::{keybundle::{generate_identity_key, generate_prekey, generate_onetime_keys, Keybundle}};
 
 #[tauri::command]
-async fn generate_keys<R: tauri::Runtime>(app: tauri::AppHandle<R>, window: tauri::Window<R>) -> Result<Keybundle<PublicKey>, String> {
+pub async fn generate_keys<R: tauri::Runtime>(app: tauri::AppHandle<R>, window: tauri::Window<R>) -> Result<Keybundle<PublicKey>, String> {
   let id_key = generate_identity_key();
   let prekey = generate_prekey(&id_key);
   let onetime_bundle = generate_onetime_keys(ONETIME_KEY_NUMBER);
