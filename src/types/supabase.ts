8f8b2b37ae27bfdacad76daccd891728594fcdea
kawuka <kawuka@kawuka.xyz>
2023-03-02 19:09:06 +0100

    onetime-key procedure types

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -157,7 +157,12 @@ export interface Database {
       [_ in never]: never
     }
     Functions: {
-      [_ in never]: never
+      get_onetime_key: {
+        Args: {
+          user_id: string
+        }
+        Returns: string
+      }
     }
     Enums: {
       [_ in never]: never
