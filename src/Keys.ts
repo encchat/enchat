011b8b5654f1f9406e2b4be1a5539a5f58856030
kawuka <kawuka@kawuka.xyz>
2023-03-05 20:40:11 +0100

    Generic keys

--- src/Keys.ts
+++ src/Keys.ts
@@ -1,6 +1,10 @@
 import { invoke } from '@tauri-apps/api';
+import type { T } from '@tauri-apps/api/event-2a9960e7';
 import { binary_to_base58 } from 'base58-js'
+import { user } from './store';
 import { supabaseClient } from './supabase';
+
+const MAX_KEYS = 10
 
 export function decode_base58(encoded: string): Uint8Array {
     return binary_to_base58(encoded)
@@ -10,20 +14,22 @@ export function decode_base58(encoded: string): Uint8Array {
  * Class used for handling public keys
  */
 export abstract class Key {
-    private _key: Uint8Array;
+    private _key: Uint8Array | null = null;
+
     public get getKey() { return this._key }
     
     abstract shouldGenerate(userId: string): Promise<boolean>;
     abstract generate(userId: string): Promise<string>;
-    abstract fetch(userId: string): Promise<string>
+    abstract fetch(userId: string, callerId: string): Promise<string | null>
 
     public async populate(userId: string): Promise<void> {
         const {data: {user}} = await supabaseClient.auth.getUser()
-        const key = await ((user.id === userId && await this.shouldGenerate(userId)) ? this.generate(userId) : this.fetch(userId))
+        const key = await ((user.id === userId && await this.shouldGenerate(userId)) ? this.generate(userId) : this.fetch(userId, user.id))
         // TODO: Handle nulls
-        this._key = decode_base58(key)
+        this._key &&= decode_base58(key)
     }
 }
+
 
 /**
  * Public identity key
@@ -31,18 +37,20 @@ export abstract class Key {
 export class IdentityKey extends Key {
     async shouldGenerate(userId: string): Promise<boolean> {
         const {error, data} = await supabaseClient.from('identity-key').select('id').eq('id', userId).single()
-        console.log(data)
         return !!error
     }
     async generate(userId: string): Promise<string> {
+        console.debug('Generating new identity key')
         const key = await invoke<string>('request_identity_key')
         await supabaseClient.from('identity-key').insert({
             id: userId,
             key
         })
+        console.debug('Done generating identity key')
         return key
     }
-    async fetch(userId: string): Promise<string> {
+    async fetch(userId: string, callerId: string): Promise<string | null> {
+        console.debug('Getting identity key')
         const {data} = await supabaseClient.from('identity-key').select('key').eq('id', userId).single()
         return data.key
     }
@@ -56,39 +64,79 @@ interface PrekeySerialized {
 export class Prekey extends Key {
     async shouldGenerate(userId: string): Promise<boolean> {
         const {error, data} = await supabaseClient.from('prekey').select('id, created_at').eq('id', userId).single()
-        console.log(data)
         // TODO: Add rotation
         return !!error
     }
     async generate(userId: string): Promise<string> {
+        console.debug('Generating new prekey')
         const {prekey, signature} = await invoke<PrekeySerialized>('request_prekey')
         await supabaseClient.from('prekey').insert({
             id: userId,
             key: prekey,
             signature
         })
+        console.debug('Done generating prekey')
         return prekey
         
     }
-    async fetch(userId: string): Promise<string> {
-        // TODO: Verify the signature
+    async fetch(userId: string, callerId: string): Promise<string | null> {
+        console.debug('Getting prekey')
         const {data} = await supabaseClient.from('prekey').select('key').eq('id', userId).single()
         return data.key
     }
 }
 
+interface OnetimeKeys {
+    id: number;
+    key: Uint8Array
+}
 export class OnetimeKey extends Key {
+    private _keysContainer: OnetimeKeys[]
+    private _keysToGenerate: number = 0
+
+    public get getKey(): Uint8Array {
+        return this._keysContainer[0].key
+    }
+
     async shouldGenerate(userId: string): Promise<boolean> {
-        console.log('???')
-        return false
-        // throw new Error('Method not implemented.');
+        const remainingKeys = await supabaseClient.from('onetime-key')
+            .select('*', {count: 'exact'})
+            .eq('user', userId)
+            .filter('used', 'is', 'null')
+        this._keysToGenerate = MAX_KEYS - remainingKeys.count
+        return this._keysToGenerate > 0
     }
     async generate(userId: string): Promise<string> {
-        throw new Error('Method not implemented.');
+        console.debug(`Generating ${this._keysToGenerate} onetime keys`)
+        const {data} = await supabaseClient.from('onetime-key')
+            .select('local_id')
+            .eq('user', userId)
+            .order('local_id', { ascending: false } )
+            .limit(1)
+        const lastKey = data?.length > 0 ? data[0].local_id : 0
+        const keys = await (await invoke<string[]>('request_onetime_keys', {keys: this._keysToGenerate, lastKey: lastKey}))
+        this._keysContainer = keys.map((x, i) => ({
+            key: decode_base58(x),
+            id: lastKey + i + 1
+        }))
+        const uploadPromise = keys.map((x, i) => supabaseClient.from('onetime-key').insert({
+            user: userId,
+            key: x,
+            local_id: lastKey + i + 1
+        }))
+        await Promise.all(uploadPromise)
+        console.debug('Done generating onetime keys')
+        return keys[0]
     }
-    async fetch(userId: string): Promise<string> {
+    async fetch(userId: string, callerId: string): Promise<string | null> {
+        if (callerId == userId) return null
+        console.debug(`Fetching onetime key for user ${userId}`)
         const {data, error} = await supabaseClient.rpc('get_onetime_key', {user_id: userId});
         return data
-    }
-    
+    }  
+}
+export const populateKey = async <K extends Key>(userId: string, type: { new(): K; }): Promise<K>  =>  {
+    const key = new type()
+    await key.populate(userId)
+    return key
 }
--- src/lib/KeyManager.svelte
+++ src/lib/KeyManager.svelte
@@ -1,89 +1,14 @@
 <script lang="ts">
-import type { User } from "@supabase/supabase-js";
-
-import { invoke } from "@tauri-apps/api/tauri";
-import { supabaseClient } from "src/supabase";
+import { IdentityKey, OnetimeKey, populateKey, Prekey } from "src/Keys";
 import { onMount } from "svelte";
 
 export let user: User.User
 
-const MAX_KEYS = 10
-
-interface Keybundle {
-    identity: string;
-    onetime: string[];
-    prekey: string;
-    signature: string;
-}
-
-interface OnetimeKey {
-    key: string;
-    id: number;
-}
-
-const uploadKeys = (keys: OnetimeKey[], user_id: string) => {
-    const promise = keys.map(x => supabaseClient.from('onetime-key').insert({
-        user: user.id,
-        key: x.key,
-        local_id: x.id
-    }))
-    return Promise.all(promise)
-}
-
-const setupKeys = async () => {
-    console.debug('Creating keys')
-    const keybundle = await invoke<Keybundle>('generate_keys')
-    await supabaseClient.from('identity-key').insert({
-        id: user.id,
-        key: keybundle.identity
-    })
-    await supabaseClient.from('prekey').insert({
-        id: user.id,
-        key: keybundle.prekey,
-        signature: keybundle.signature
-    })
-    await uploadKeys(keybundle.onetime.map((x,i) => ({key: x, id: i})), user.id)
-    console.debug('Uploaded keys')
-}
-
-
-const hasStoredKeys = async (): Promise<boolean> => {
-    const {data} = await supabaseClient.from('identity-key').select().eq('id', user.id)
-    return data?.length > 0
-}
-
-const getNumberOfKeysToGenerate = async (): Promise<number> => {
-    const remainingKeys = await supabaseClient.from('onetime-key')
-        .select('*', {count: 'exact'})
-        .eq('user', user.id)
-        .filter('used', 'is', 'null')
-    if (remainingKeys.error) return 0
-    return MAX_KEYS - remainingKeys.count
-}
-const generateOnetimeKeys = async (n: number) => {
-    const {data} = await supabaseClient.from('onetime-key')
-        .select('local_id')
-        .eq('user', user.id)
-        .order('local_id', { ascending: false } )
-        .limit(1)
-    const lastKey = data?.length > 0 ? data[0].local_id : 0
-    console.debug(`Refreshing ${n} onetime keys`)
-    const keys = await invoke<string[]>('request_onetime_keys', {keys: n, lastKey: lastKey})
-    await uploadKeys(keys.map((x,i) => ({key: x, id: i + 1 + lastKey}) ), user.id)
-    console.debug(`Uploaded ${n} onetime keys`)
-}
-
-const removeUsedKeys = async () => {
-    return supabaseClient.from('onetime-key').delete().eq('used', true).eq('user', user.id)
-}
 
 onMount(async () => {
-    if (!await hasStoredKeys())
-        return await setupKeys()
-    const keys = await getNumberOfKeysToGenerate()
-    if (keys > 0)
-        await generateOnetimeKeys(keys)
-    await removeUsedKeys()
+    await populateKey(user.id, IdentityKey)
+    await populateKey(user.id, Prekey)
+    await populateKey(user.id, OnetimeKey)
 })
 
 </script>
