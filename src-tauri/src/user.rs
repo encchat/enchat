5128bab62502f864a35ebfd037c88e7ca35ff103
kawuka <kawuka@kawuka.xyz>
2023-03-17 19:41:24 +0100

    Clear state in rust just to be sure

--- src-tauri/src/user.rs
+++ src-tauri/src/user.rs
@@ -2,6 +2,8 @@ use std::sync::Mutex;
 
 use tauri::State;
 
+use crate::chat::WrappedChatState;
+
 #[derive(Default)]
 pub struct User {
     pub user_id: Option<String>
@@ -10,7 +12,9 @@ pub struct User {
 pub struct UserState(pub Mutex<User>);
 
 #[tauri::command]
-pub fn login(user_id: String, user_state: State<UserState>) {
+pub fn login(user_id: String, user_state: State<UserState>, chat_state: State<WrappedChatState>) {
+    let mut chat = chat_state.0.lock().unwrap();
     let mut user = user_state.0.lock().unwrap();
+    *chat = Default::default();
     (*user).user_id = Some(user_id);
 }
