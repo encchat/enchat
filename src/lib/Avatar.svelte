042fbd106981342e4e89d66ea562807bdc7ecc63
korneliuszw <korneliuszwstudio@gmail.com>
2023-03-29 15:52:04 +0200

    Release fixes - bundle background and avatar, body overflow

--- src/lib/Avatar.svelte
+++ src/lib/Avatar.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 import { supabaseClient } from "src/supabase";
+import defaultAvatar from '/public/tauri.svg'
 export let avatarUrl: string | null = null;
-let url: string = "public/tauri.svg"
+let url: string | null = null
 const downloadImage = async (path: string) => {
     console.log('download')
     const {data, error} = await supabaseClient.storage.from('avatars').download(path)
@@ -12,4 +13,4 @@ $:
     if (avatarUrl) downloadImage(avatarUrl)
 </script>
 
-<img src={url} class="rounded-full h-7 w-7 border border-transparent  object-fill" alt="user avatar">
+<img src={url ?? defaultAvatar} class="rounded-full h-7 w-7 border border-transparent  object-fill" alt="user avatar">
